c150e4b2d795d14b9faca1c54fee975fe9b7810a
Jan Hadrava <had@kam.mff.cuni.cz>
2013-08-27 23:11:56 +0200

    reverse transform sph_image to spherical

--- src/transform.c
+++ src/transform.c
@@ -8,3 +8,9 @@ CvPoint sph_to_sph_image(CvPoint2D64f spherical){
                           par_sph_height/2 - par_sph_height * spherical.y / 10800);
   return image;
 }
+
+CvPoint2D64f sph_image_to_sph(CvPoint image){
+  CvPoint2D64f spherical = cvPoint2D64f(21600 * image.x / par_sph_width,
+                                        (par_sph_height/2 - image.y) * 10800 / par_sph_height);
+  return spherical;
+}
--- src/transform.h
+++ src/transform.h
@@ -2,5 +2,6 @@
 #define TRANSFORM_H_
 
 CvPoint sph_to_sph_image(CvPoint2D64f spherical);
+CvPoint2D64f sph_image_to_sph(CvPoint image);
 
 #endif
