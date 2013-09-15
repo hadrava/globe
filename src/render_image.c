9cf767b61d98fae34f2dcf41269e291c0b7bff1c
Jan Hadrava <had@kam.mff.cuni.cz>
2013-09-15 15:07:38 +0200

    image window working correctly

--- src/render_image.c
+++ src/render_image.c
@@ -7,25 +7,29 @@
 #include "render.h"
 #include "render_image.h"
 
-static long long int render_image_threads = 0;//TODO void hack
+static int render_image_threads = 0;
 static pthread_t *render_tids;
+static int *render_thread_numbers;
 static pthread_barrier_t render_barrier_run;
 static pthread_barrier_t render_barrier_finish;
 static volatile unsigned char render_thread_running;
 
 void *render_thread_main(void *arg) {
-  while (render_thread_running) {
+  while (1) {
     pthread_barrier_wait(&render_barrier_run);
-    render_images_threaded((long long int) arg);//TODO void hack
+    if (!render_thread_running)
+      break;
+    render_images_threaded(*(int *)arg, 0);
     pthread_barrier_wait(&render_barrier_finish);
   }
   pthread_exit(NULL);
 }
 
-void render_images_threaded(int line) {
+void render_images_threaded(int line, int verbose) {
   struct image_list * list = image_list_head;
   while (list) {
-    image_params_print(&list->params);
+    if (verbose)
+      image_params_print(&list->params);
     struct projection_params proj_params;
     sph_to_image_precalculate_projection(&list->params, list->image->width, list->image->height, &proj_params);
 
@@ -49,12 +53,12 @@ void render_images_window() {
   struct image_list * list = image_list_head;
   while (list) {
     if (list->display_window) {
-  //    cvResize(list->image, list->window_image, CV_INTER_CUBIC);
- //     struct projection_params proj_params;
-  //    sph_to_image_precalculate_projection(&list->params, list->image->width, list->image->height, &proj_params);
-   ////     CvPoint position = sph_to_image(sph_image_to_sph(cvPoint(i,j)), &list->params, &proj_params);
-////	star
-   //   cvShowImage(list->filename, list->window_image);
+      cvResize(list->image, list->window_image, CV_INTER_CUBIC);
+      struct projection_params proj_params;
+      sph_to_image_precalculate_projection(&list->params, list->image->width, list->image->height, &proj_params);
+   //     CvPoint position = sph_to_image(sph_image_to_sph(cvPoint(i,j)), &list->params, &proj_params);
+//	star
+      cvShowImage(list->filename, list->window_image);
     }
     list = list->next;
   }
@@ -63,12 +67,14 @@ void render_images_window() {
 void render_image_init(int threads) {
   render_image_threads = 0;
   render_tids = malloc((threads-1)*sizeof(pthread_t));
+  render_thread_numbers = malloc((threads-1)*sizeof(int));
   pthread_barrier_init(&render_barrier_run, NULL, threads);
   pthread_barrier_init(&render_barrier_finish, NULL, threads);
   render_thread_running = 1;
   while (--threads) {
     pthread_attr_t *thAttr = NULL;
-    pthread_create(render_tids+render_image_threads, thAttr, render_thread_main, (void *) render_image_threads);//TODO void hack
+    render_thread_numbers[render_image_threads] = render_image_threads;
+    pthread_create(render_tids+render_image_threads, thAttr, render_thread_main, render_thread_numbers+render_image_threads);//TODO void hack
     render_image_threads++;
   }
 
@@ -76,9 +82,8 @@ void render_image_init(int threads) {
 }
 
 void render_image_close() {
-  pthread_barrier_wait(&render_barrier_run);
   render_thread_running = 0;
-  pthread_barrier_wait(&render_barrier_finish);
+  pthread_barrier_wait(&render_barrier_run);
   while (render_image_threads--) {
     void *loop_return;
     pthread_join(render_tids[render_image_threads], &loop_return);
@@ -88,7 +93,7 @@ void render_image_close() {
 void render_images() {
   cvZero(sph_image);
   pthread_barrier_wait(&render_barrier_run);
-  render_images_threaded(render_image_threads);
+  render_images_threaded(render_image_threads, 1);
   pthread_barrier_wait(&render_barrier_finish);
-//  render_images_window();
+  render_images_window();
 }
--- src/render_image.h
+++ src/render_image.h
@@ -2,7 +2,7 @@
 #define RENDER_IMAGE_H_
 
 void *render_thread_main(void *arg);
-void render_images_threaded(int line);
+void render_images_threaded(int line, int verbose);
 void render_images_window();
 void render_image_init(int threads);
 void render_image_close();
